4b31c1ccf0ad0e152afb4dc14920e91dd3ebb83e
lextri980 <lextri980@gmail.com>
2023-05-29 20:23:12 +0700

    update db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    mongoose.connect(process.env.DB_CONNECT, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.fobqfgi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connected!");
   } catch (error) {
     console.log(error);
