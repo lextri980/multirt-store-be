285d5fbfb5207d2d1453e0c809d9564d6c15d98b
lextri980 <lextri980@gmail.com>
2023-07-02 15:46:05 +0700

    update

--- src/middleware/protectedRoute.js
+++ src/middleware/protectedRoute.js
@@ -12,7 +12,9 @@ const protectedRoute = async (req, res, next) => {
     token = req.headers.authorization.split(" ")[1];
     try {
       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.id)
+        .select("-password")
+        .populate("role");
       next();
     } catch (error) {
       if (error.expiredAt) {
